6d700de563741a038c12c83611fa2eb8df582533
Joost <113507203+JoostKraan@users.noreply.github.com>
2025-06-14 23:08:51 +0200

    Fixed bottom app bar and blur effects
    Also added new material3 icons for now
    and darkmode toggle button

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,24 +1,28 @@
+import 'dart:ui';
+
 import 'package:app/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:app/services.dart';
 import 'package:one_clock/one_clock.dart';
-import 'package:blur/blur.dart';
+
 
 Services services = Services();
 void main() {
   runApp(const MyApp());
   initialize();
-
 }
+
 void initialize() {
   services.checkForInternet();
-
 }
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,13 +30,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),fontFamily: 'Manrope'
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Manrope',
+        dividerTheme: const DividerThemeData(
+          color: Colors.transparent,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -44,108 +52,158 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool darkmode = true;
+
+  void toggleDarkmode() {
+    setState(() {
+      darkmode = !darkmode;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     final double containerWidth = screenSize.width / 3;
+    final LatLng carLocation = LatLng(52.68697352961828, 6.60489107953327);
+
+    final constants = Constants(darkmode);
+
 
     return Scaffold(
-        extendBodyBehindAppBar: true,
-      appBar: PreferredSize(
+      backgroundColor: constants.primaryColor,
 
-        preferredSize: Size.fromHeight(50.0),
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: Constants.primaryColor,
-          flexibleSpace: Stack(
-            children: [
-              Blur(
-                blur: 2,
-                blurColor: Constants.primaryColor,
-                child: Container(color: Constants.primaryColor.withOpacity(0.01)),
-              ),
-            ],
-          ),
-
-          title: Row(
-            children: [
-              Text("47L",style: TextStyle(color: Constants.fontColor,fontSize:  Constants.textSize)),
-
-            ],
-          ),
-
-          actions: [
-            Padding(padding: EdgeInsets.only(right: 10),
-              child: ValueListenableBuilder(valueListenable: services.hasInternet, builder: (context,hasInternet,child){
-                return SvgPicture.asset(hasInternet ? 'assets/icons/wifi.svg' : 'assets/icons/wifi-off.svg',
-                    color: Constants.iconColor,
-                    width: Constants.iconSize,
-                    height: Constants.iconSize);
-              }
-              )
-            ),
-            Padding(padding: EdgeInsets.only(right: 10),
-              child:SvgPicture.asset('assets/icons/bluetooth.svg',
-                color: Constants.iconColor,
-                width: Constants.iconSize,
-                height: Constants.iconSize,
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(right: 10),
-              child:SvgPicture.asset('assets/icons/satellite-variant.svg',
-                color: Constants.iconColor,
-                width: Constants.iconSize,
-                height: Constants.iconSize,
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(right: 10),
-              child: DigitalClock(
-                digitalClockTextColor: Constants.fontColor,
-
-                  format: "HH:mm",
-                  showSeconds: false,
-                  isLive:true,),
-            ),
-          ],
-        ),
-      ),
+      extendBodyBehindAppBar: true,
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Positioned.fill(child: FlutterMap(
-            options: MapOptions(
-              initialCenter: const LatLng(1, 1),
-              initialZoom: 3.2,
-            ),
-            children: [
-              TileLayer(
-                retinaMode: true,
-                urlTemplate: 'https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=ME95gmQBq6fVpZys7OtD6VJLMx706vzQRALB4oZiea5VnbQ7rfH9xjiOIu5wyy5b',
-                userAgentPackageName: 'com.example.app',
+          Positioned.fill(
+            child: FlutterMap(
+              options: MapOptions(
+                initialCenter: const LatLng(1, 1),
+                initialZoom: 3.2,
+              ),
+              children: [
+                TileLayer(
+                  retinaMode: true,
+                  urlTemplate: constants.mapurl,
+                  userAgentPackageName: 'com.example.app',
+                ),
+                Positioned(
+                  bottom: 50,
+                  right: 0,
+                  child: SimpleAttributionWidget(
+                    backgroundColor: Color(0xFFFFFF),
+                    source: Text(
+                      style: TextStyle(color: constants.iconColor),
+                      'OpenStreetMap contributors',
+                    ),
+                  ),
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: carLocation,
+                      width: 50,
+                      height: 50,
+                      child: SvgPicture.asset('assets/icons/Ford-logo-flat.svg'),
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
-          Positioned(top: 50,
-              left: 0,
-              width: containerWidth,
-              height: screenSize.height,
-              child: SizedBox(
-                child:
-              Blur(
-                blurColor: Constants.primaryColor,
-                blur: 2,
-                child: Container(
-                  color: Constants.primaryColor,
-                  child: Text("Test"),
+          Positioned(
+            top: 10,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: ValueListenableBuilder(
+                    valueListenable: services.hasInternet,
+                    builder: (context, hasInternet, child) {
+                      return SvgPicture.asset(
+                        hasInternet
+                            ? 'assets/material3icons/wifi.svg'
+                            : 'assets/material3icons/wifi-off.svg',
+                        color: constants.iconColor,
+                        width: constants.iconSize,
+                        height: constants.iconSize,
+                      );
+                    },
                   ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: SvgPicture.asset(
+                    'assets/material3icons/bluetooth-disabled.svg',
+                    color: constants.iconColor,
+                    width: constants.iconSize,
+                    height: constants.iconSize,
+                  ),
+                ),
+                SvgPicture.asset(
+                  'assets/material3icons/signal-cellular-off.svg',
+                  color: constants.iconColor,
+                  width: constants.iconSize,
+                  height: constants.iconSize,
+                ),
+                DigitalClock(
+                  digitalClockTextColor: constants.fontColor,
+                  format: "HH:mm",
+                  showSeconds: false,
+                  isLive: true,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 10,
+            top: 50,
+            child: Column(
+              children: [
+                IconButton(
+                  onPressed: toggleDarkmode,
+                  icon: SvgPicture.asset(
+                      color: constants.iconColor,
+                      'assets/icons/theme-light-dark.svg'),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            left: 0,
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                child: Container(
+                  width: containerWidth,
+                  color: constants.primaryColor,
+                ),
               ),
-              )
-          )
-
-        ]
-      )
+            ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            left: 421,
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                child: Container(
+                  width: screenSize.width,
+                  height: 50,
+                  color: constants.primaryColor,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- app/lib/theme.dart
+++ app/lib/theme.dart
@@ -1,14 +1,30 @@
 import 'dart:ui';
 
 class Constants {
-  static const primaryColor = Color(0xD10E1314);
-  static const secondaryColor = Color(0xFF333333);
-  static const accentColor = Color(0xFF0E79B2);
-  static const errorColor = Color(0xFFEF476F);
-  static const successColor = Color(0xFF009B72);
-  static const fontColor = Color(0xFFFFFFFF);
-  static const iconColor = Color(0xFFFFFFFF);
 
-  static double iconSize = 25;
-  static double textSize = 16;
+  final Color primaryColor;
+  final Color secondaryColor;
+  final Color accentColor;
+  final Color errorColor;
+  final Color successColor;
+  final Color fontColor;
+  final Color iconColor;
+
+  final double iconSize;
+  final double textSize;
+  final String mapurl;
+
+
+  Constants(bool dark)
+      : primaryColor = dark ? const Color(0xBE1A1A1A) : const Color(0x83F6F6F4),
+        secondaryColor = dark ? const Color(0xFF333333) : const Color(0xD19B9B9B),
+        accentColor = const Color(0xFF0E79B2),
+        errorColor = const Color(0xFFEF476F),
+        successColor = const Color(0xFF009B72),
+        fontColor =  dark ? const Color(0xFFFFFFFF) : const Color(0xD10E1314)  ,
+        iconColor =  dark ? const Color(0xFFFFFFFF) : const Color(0xD10E1314),
+        iconSize = 25,
+        textSize = 16,
+        mapurl = dark ? "https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=ME95gmQBq6fVpZys7OtD6VJLMx706vzQRALB4oZiea5VnbQ7rfH9xjiOIu5wyy5b"
+                      : "https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=ME95gmQBq6fVpZys7OtD6VJLMx706vzQRALB4oZiea5VnbQ7rfH9xjiOIu5wyy5b";
 }
